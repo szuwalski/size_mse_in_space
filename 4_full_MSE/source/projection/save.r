e8a39362bfb9d64c94d6e1699ea6fb14b4b49a2d
balglave <57452072+balglave@users.noreply.github.com>
2023-06-27 14:28:22 +0200

    modif

--- 4_full_MSE/source/projection/save.r
+++ 4_full_MSE/source/projection/save.r
@@ -7,17 +7,17 @@ if(!dir.exists(save_file_name)) dir.create(save_file_name)
 
 save(data=follow_ab_df,file = paste0(save_file_name,"follow_ab_df.RData"))
 
-imm_N_at_Len_full[[t]] = temp_imm_N
-save(data=imm_N_at_Len_full,file = paste0(save_file_name,"imm_N_at_Len_full.RData"))
+# imm_N_at_Len_full[[t]] = temp_imm_N
+# save(data=imm_N_at_Len_full,file = paste0(save_file_name,"imm_N_at_Len_full.RData"))
 
-mat_N_at_Len_full[[t]] = temp_mat_N
-save(data=temp_mat_N,file = paste0(save_file_name,"temp_mat_N.RData"))
+# mat_N_at_Len_full[[t]] = temp_mat_N
+# save(data=temp_mat_N,file = paste0(save_file_name,"temp_mat_N.RData"))
 
 total_spatial_catch_full[[t]] = total_spatial_catch
 save(data=total_spatial_catch,file = paste0(save_file_name,"total_spatial_catch.RData"))
 
-catch_by_fisher_full[[t]] = catch_by_fisher
-save(data=catch_by_fisher,file = paste0(save_file_name,"catch_by_fisher.RData"))
+# catch_by_fisher_full[[t]] = catch_by_fisher
+# save(data=catch_by_fisher,file = paste0(save_file_name,"catch_by_fisher.RData"))
 # 
 # profit_by_fisher_full[[t]] = profit_by_fisher
 # save(data=profit_by_fisher,file = paste0(save_file_name,"profit_by_fisher.RData"))
